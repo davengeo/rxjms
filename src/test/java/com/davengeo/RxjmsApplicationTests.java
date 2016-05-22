b53903684f1e34ad22ba4f31c03deba6ada22e6b
David Navarro Alvarez <davengeo@yahoo.es>
2016-05-22 21:16:51 +0200

    Initial commit

---
+++ src/test/java/com/davengeo/RxjmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.davengeo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RxjmsApplication.class)
+public class RxjmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
